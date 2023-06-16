8e1650ff385a9c3ac887cc9dd44b129a0107817e
aryan1403 <aaryan14032006@email.com>
2023-06-16 15:59:47 +0530

    updated

--- server.js
+++ server.js
@@ -2,13 +2,34 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+app.use(express.json())
+
 // REQUEST / RESPONSE
 app.get('/', (req, res) => {
-    res.end('Application Started enjoy ;)');
+    // res.end('Application Started enjoy ;)');
+    res.send('<h1>click me<h1/>');
 });
 
 app.get('/cart', (req, res) => {
     res.end('Welcome to the cart');
+});
+
+app.post('/login', (req, res) => {
+    const body = req.body;
+    console.log(body.username, body.pass);
+
+    // store in db
+
+    res.end('logged in successfully')
+});
+
+app.post('/register', (req, res) => {
+    const body = req.body;
+    console.log(body.username, body.pass);
+
+    // store in db
+
+    res.end('registered in successfully')
 });
 
 app.listen(PORT, () => console.log("Server started at port", 8080));
